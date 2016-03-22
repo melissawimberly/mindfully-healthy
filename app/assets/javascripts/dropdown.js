332fea9e10c2466d50d7b9595a9d4813fa2362cd
melissawimberly <melissa.f.wimberly@gmail.com>
2016-03-22 00:26:54 -0400

    Update carousel with working images

---
+++ app/assets/javascripts/dropdown.js
@@ -0,0 +1,11 @@
+var philosophyLink = $('#philosophy-link');
+var recipesLink = $('#philosophy-link');
+var lifestyleLink = $('#philosophy-link');
+var mediationLink = $('#philosophy-link');
+var addLink = $('#philosophy-link');
+
+
+philosophyLink.on('click', function (e) {
+	e.preventDefault();
+	console.log('clicked the damn thang!')
+});
