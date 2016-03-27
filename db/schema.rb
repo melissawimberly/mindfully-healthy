aa3552a0f89f0d3be089e49ea1df85bdfea6b981
melissawimberly <melissa.f.wimberly@gmail.com>
2016-03-27 13:24:43 -0400

    Add like model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160314235434) do
+ActiveRecord::Schema.define(version: 20160327172233) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body",       limit: 65535
     t.integer  "user_id",    limit: 4
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "recipe_id",  limit: 4
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   create_table "recipes", force: :cascade do |t|
