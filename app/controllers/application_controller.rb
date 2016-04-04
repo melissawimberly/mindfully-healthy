b3778a94ff726e4e373312754304b4081971380d
melissawimberly <melissa.f.wimberly@gmail.com>
2016-04-04 19:27:01 -0400

    Add single auth page with modals

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
 
      # binding.pry
-     root_path
+     recipes_path
    end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
 	before_filter :authenticate_user!
 	
+
 	def index
 		@recipes = Recipe.all
 		@likes = []
