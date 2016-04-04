b80ae4900da844d0c0ad0ef1dbf8cf1f7103aad6
melissawimberly <melissa.f.wimberly@gmail.com>
2016-04-04 18:18:44 -0400

    Remove comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,6 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-  	 # binding.pry
-     #puts "this shit happened....!!!!!!!"
-     #raise "foo"
-     # redirect_to root_path
 
      binding.pry
      root_path
