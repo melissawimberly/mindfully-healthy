294dc3d9d5da32a7d00fa91bc6dc597b333976ec
melissawimberly <melissa.f.wimberly@gmail.com>
2016-04-29 15:26:27 -0400

    Update controller to properly show each kind of recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -63,34 +63,30 @@ class RecipesController < ApplicationController
 		@recipes = Recipe.all
 		@breakfastrecipes = []
 
-		breakfast = Recipe.where(type: 'breakfast')
+		breakfast = Recipe.where(:kind => 'breakfast')
 		breakfast.each do  |single_breakfast|
-			@breakfastrecipes << single_breakfast.name 
+			@breakfastrecipes << single_breakfast 
 		end
 	end
 
 	def snacksindex
 		@recipes = Recipe.all
 		@snackrecipes = []
-		snacks = Recipe.where(:type => "snacks")
-		# snacks = Recipe.where(type: 'snacks')
+		
+		snacks = Recipe.where(:kind => "snacks")
 		snacks.each do  |single_snack|
-			@snackrecipes << single_snack.name 
+			@snackrecipes << single_snack
 		end
-		# @recipes.each do |recipe|
-		# 	if recipe.type = 'snacks'
-		# 		@snackrecipes << recipe
-		# 	end
-		# end
+		
 	end
 
 	def sweetsindex
 		@recipes = Recipe.all
 		@sweetrecipes = []
 
-		sweets = Recipe.where(type: 'sweets')
+		sweets = Recipe.where(:kind => 'sweets')
 		sweets.each do  |single_sweet|
-			@sweetrecipes << single_sweet.name 
+			@sweetrecipes << single_sweet 
 		end
 	end
 
@@ -98,9 +94,9 @@ class RecipesController < ApplicationController
 		@recipes = Recipe.all
 		@mmrecipes = []
 
-		mm = Recipe.where(type: 'mm')
+		mm = Recipe.where(:kind => 'mm')
 		mm.each do  |single_mm|
-			@mmrecipes << single_mm.name 
+			@mmrecipes << single_mm
 		end
 	end
 
@@ -110,7 +106,7 @@ class RecipesController < ApplicationController
 
 	private
 		def recipe_params
-			params.require(:recipe).permit(:name, :recipe, :type, :pic, :user_id, :rec_id)
+			params.require(:recipe).permit(:name, :recipe, :kind, :pic, :user_id, :rec_id)
 		end
 end
 
