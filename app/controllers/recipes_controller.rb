7a2d18d5e700edd470855c092a4fffb7b1c5bb80
melissawimberly <melissa.f.wimberly@gmail.com>
2016-05-01 23:38:32 -0400

    Comment removal

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -33,8 +33,7 @@ class RecipesController < ApplicationController
 				@like = Like.create(
 					recipe_id: params[:rec_id],
 					user_id: current_user.id
-					)
-		        # render json: {liked: true}
+				)
 		    }
 		end
 
@@ -47,67 +46,61 @@ class RecipesController < ApplicationController
 				@like = Like.where(
 					recipe_id: params[:rec_id],
 					user_id: current_user.id
-					)
-		      	# render json: {unliked: true}
+				)
 		      	@like.delete_all
-
-		      }
+		     }
 		  end
-		end
+	end
 
-		def likesindex
-			@recipes = current_user.recipes.uniq
+	def likesindex
+		@recipes = current_user.recipes.uniq
 
-		end
+	end
 
-		def breakfastindex
-			@recipes = Recipe.all
-			@breakfastrecipes = []
+	def breakfastindex
+		@recipes = Recipe.all
+		@breakfastrecipes = []
 
-			breakfast = Recipe.where(:kind => 'breakfast')
-			breakfast.each do  |single_breakfast|
-				@breakfastrecipes << single_breakfast 
-			end
-		end
-
-		def snacksindex
-			@recipes = Recipe.all
-			@snackrecipes = []
-			
-			snacks = Recipe.where(:kind => "snacks")
-			snacks.each do  |single_snack|
-				@snackrecipes << single_snack
-			end
-			
-		end
-
-		def sweetsindex
-			@recipes = Recipe.all
-			@sweetrecipes = []
-
-			sweets = Recipe.where(:kind => 'sweets')
-			sweets.each do  |single_sweet|
-				@sweetrecipes << single_sweet 
-			end
-		end
-
-		def mmsindex
-			@recipes = Recipe.all
-			@mmrecipes = []
-
-			mm = Recipe.where(:kind => 'mm')
-			mm.each do  |single_mm|
-				@mmrecipes << single_mm
-			end
-		end
-
-		def about
-			redirect_to :about
-		end
-
-		private
-		def recipe_params
-			params.require(:recipe).permit(:name, :recipe, :kind, :pic, :user_id, :rec_id)
+		breakfast = Recipe.where(:kind => 'breakfast')
+		breakfast.each do  |single_breakfast|
+			@breakfastrecipes << single_breakfast 
 		end
 	end
+
+	def snacksindex
+		@recipes = Recipe.all
+		@snackrecipes = []
+
+		snacks = Recipe.where(:kind => "snacks")
+		snacks.each do  |single_snack|
+			@snackrecipes << single_snack
+		end
+	end
+
+	def sweetsindex
+		@recipes = Recipe.all
+		@sweetrecipes = []
+
+		sweets = Recipe.where(:kind => 'sweets')
+		sweets.each do  |single_sweet|
+			@sweetrecipes << single_sweet 
+		end
+	end
+
+	def mmsindex
+		@recipes = Recipe.all
+		@mmrecipes = []
+
+		mm = Recipe.where(:kind => 'mm')
+		mm.each do  |single_mm|
+			@mmrecipes << single_mm
+		end
+	end
+
+
+	private
+	def recipe_params
+		params.require(:recipe).permit(:name, :recipe, :kind, :pic, :user_id, :rec_id)
+	end
+end
 
